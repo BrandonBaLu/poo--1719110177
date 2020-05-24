da5fdd9ae292289975f4e5ab0289af07612b58a2
Brandon Balderas Lucas <1719110177@utectulancingo.edu.mx>
2020-05-24 21:24:02 +0000

    Terminado Azteca.py

---
+++ semana-2/Azteca.py
@@ -0,0 +1,36 @@
+class Azteca:
+
+  """Atríbutos"""
+  cajeros= 10
+  ventanillas= 10
+  personal= 30
+  sillas= 24
+  maquina_de_turnos=1
+  pantallas=3
+  bocinas=3
+  oficinas=10
+  computadoras=20
+  impresoras=2
+  
+  """Métodos"""
+  def metodos():
+    print("crear cuentas bancarias")
+    print("ahorrar")
+    print("depositar")
+    print("retirar")
+    print("pagar servicios")
+
+    def _init_(self):
+      print("atríbutos del banco Azteca")
+      print(self.cajeros)
+      print(self.ventanillass)
+      print(self.personal)
+      print(self.sillas)
+      print(self.maquina_de_turnos)
+      print(self.pantallas)
+      print(self.oficinas)
+      print(self.computadoras)
+      print(self.impresoras)
+      self.metodos()
+
+objeto=Azteca
