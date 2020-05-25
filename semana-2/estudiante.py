a6afea2a80aa560eab4b283399f935691b0cb330
Brandon Balderas Lucas <1719110177@utectulancingo.edu.mx>
2020-05-25 01:55:40 +0000

    1.1

--- semana-2/estudiante.py
+++ semana-2/estudiante.py
@@ -1,34 +1,47 @@
-class Banco:
+class estudiante:
+  '''Atríbutos'''
 
-  cajeros= 5
-  ventanillas= 7
-  empleados= 30
-  camarasseguridad= 10
-  alarmas= 3
-  sucursales= 6
-  oficinas= 6
+  nombre= "Brandon Balderas Lucas"
+  matricula= "1719110177"
+  cursa= "ingenieria en Tics"
+  grupo= "tic 22"
+  promedio= "9.66"
+  materias= "7"
+  regular= "si"
+  taller= "guitarra"
+  computadoras= "1"
+  impresoras="1"
  
-  
-  def servicioBanacario(self):
-    print("servicioBanacario")
-  def prestamos(self):
-    print("prestamos")
-  def seguridad(self):
-    print("seguridad")
+  '''Métodos'''
+  def estudiar(self):
+    print("estudiar una carrera")
+  def entregar(self):
+    print("entregar tareas")
+  def calificar(self):
+    print("calificar")
+  def reinscribirse(self):
+    print("reinscribirse")
+  def pagar(self):
+    print("pagar en el banco")  
 
-  def __init__(self):
-    print("Banco Banamex")
-    print(self.cajeros)
-    print(self.ventanillas)
-    print(self.empleados)
-    print(self.camarasseguridad)
-    print(self.alarmas)
-    print(self.sucursales)
-    print(self.oficinas)
-    
-objeto = Banco()
-objeto.servicioBanacario()
-objeto.seguridad()
-objeto.prestamos()
+  def _init_(self):
+    print("atributos de un alumno")
+    print("Nombre="+str(self.nombre))
+    print("Matricula"+str(self.matricula))
+    print("Cursa="+str (self.cursa))
+    print("grupo="+str(self.grupo))
+    print("promedio="+str(self.promedio))
+    print("materias="+str(self.materias))
+    print("regular="+str(self.regular))
+    print("taller="+str(self.taller))
+    print("computadoras="+str(self.computadoras))
+    print("impresoras="+str(self.impresoras))
+objeto = estudiante()
+objeto.estudiar()
+objeto.entregar()
+objeto.calificar()
+objeto.reinscribirse()
+objeto.pagar()
+objeto._init_()
 
 
