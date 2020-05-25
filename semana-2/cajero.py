f575da790fe9857fea6d2d4d39c159faf72f8645
Brandon Balderas Lucas <1719110177@utectulancingo.edu.mx>
2020-05-25 04:43:10 +0000

    1.2

---
+++ semana-2/cajero.py
@@ -0,0 +1,36 @@
+class futbolista:
+  funciones= 22
+  gabeta= "si"
+  gabeta_billetes= 1
+  retirar= "cantidad a retirar"
+  depositar="juvenil"
+  nombre= "Brandon BaLu"
+  
+  def imprimir(self):
+    print("imprime")
+  def cambio(self):
+    print("cambiar dinero")
+  def servicios(self):
+    print("abre gabeta")
+  def pagar(self):
+    print("cierra gabeta")
+  def resivir(self):
+    print("contar dinero")
+ 
+  def _init_(self):
+    print("Futbolista")
+    print(self.funciones)
+    print(self.gabeta)
+    print(self.gabeta_billetes)
+    print(self.retirar)
+    print(self.depositar)
+    print(self.nombre)
+
+objeto = futbolista()
+objeto.imprimir()
+objeto.cambio()
+objeto.servicios()
+objeto.pagar()
+objeto.resivir()
+objeto._init_()
+
---
+++ semana-2/calculadora.py
@@ -0,0 +1,40 @@
+class calculadora:
+  funciones= 500
+  multiplicacion="multiplicacion"
+  teclas= 50
+  tipo= "cientifica"
+  Modelo= "STR25"
+  marca= "CASIO"
+  tama= "grande"
+  forma="rectangular"
+  color="roja"
+  '''metodos'''
+  def prender(self):
+    print("Encender")
+  def multiplica(self):
+    print("multiplica") 
+  def sumar(self):
+    print("sumar")
+  def restar(self):
+    print("restar")
+  def apagar(self):
+    print("Apagado automatico")
+  
+  def _init_(self):
+    print("Calculadora Cientifica")
+    print("funciones"+ str(self.funciones))
+    print(self.multiplicacion)
+    print(self.teclas)
+    print(self.tipo)
+    print(self.modelo)
+    print(self.marca)
+    print(self.tama)
+    print(self.forma)
+    print(self.color)
+    
+objeto=calculadora()
+objeto.prender
+objeto.multiplica
+objeto.sumar
+objeto.restar
+objeto._init_
