c0469a5aee2d6d390bc0aa45f6b0c34e76fae62a
Brandon Balderas Lucas <1719110177@utectulancingo.edu.mx>
2020-05-28 15:10:32 +0000

    1.2

--- semana-3/azteca.py
+++ semana-3/azteca.py
@@ -1,10 +1,46 @@
-class azteca:
+class banco:
    
+   "atributos"
+   cajeros: 5
+   personal: 30
+   sillas: 15
+   pantallas_llama_turnos= 4
+   ventanillas= 12
    
    def __init__(self):
      print("Constructur de bancos")
 
   def retirar(self):
-    print depositar(self):
+    print("retirar")
+
+  def depositar(self):
+    print("depositar")
+    
+    
+class azteca(banco):
+
+  def
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     
