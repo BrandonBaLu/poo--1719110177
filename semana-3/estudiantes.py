db17d33e7aa8b5f354dd964be9eace2b201a645d
Brandon Balderas Lucas <1719110177@utectulancingo.edu.mx>
2020-05-28 17:50:46 +0000

    2

--- semana-3/estudiantes.py
+++ semana-3/estudiantes.py
@@ -1,15 +1,24 @@
 class estudiante:
    
   "atributos"
-  cajeros= 5
-  personal= 30
+  uniforme= 5
+  mochila = 30
   sillas= 15
   pantallas_llama_turnos= 4
   ventanillas= 12
-   
+
+  "atributos" 
   def __init__(self):
      print("Constructor de bancos")
+     print("cajeros= "+str(self.cajeros))
+     print("personal= "+str(self.personal))
+     print("sillas= "+str(self.sillas))
+     print("pantallas llama turnos= "+str(self.pantallas_llama_turnos))
+     print("ventanillas= "+str(self.ventanillas))
 
+  
+  
+  "métodos"
   def retirar(self):
     print("retirar")
 
@@ -17,17 +26,23 @@ class estudiante:
     print("depositar")
     
     
+
 class azteca(banco):  
   
   def __init__(self):
-    print("constructor de banco Azteca" )
+    print("constructor de banco Azteca")
+    print("cajeros= "+str(self.cajeros))
+    print("personal= "+str(self.personal))
+    print("sillas= "+str(self.sillas))
+    print("pantallas llama turnos= "+str(self.pantallas_llama_turnos))
+    print("ventanillas= "+str(self.ventanillas))
 
-objeto=estudiante()
+
+
+objeto = banco()
 objeto.retirar()
 objeto.depositar()
 
-objeto_azteca=azteca()
-objeto_azteca.retirar
-objeto.retirar()
-objeto.depositar()
-objeto.__init__()
+objeto_azteca = azteca()
+objeto_azteca.retirar()
+objeto_azteca.depositar()
