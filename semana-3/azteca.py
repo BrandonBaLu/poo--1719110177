49ab61a8abcc77879e6e78444301ce8316120131
Brandon Balderas Lucas <1719110177@utectulancingo.edu.mx>
2020-05-28 15:25:08 +0000

    1.1

--- semana-3/azteca.py
+++ semana-3/azteca.py
@@ -1,11 +1,11 @@
 class banco:
    
-   "atributos"
-   cajeros: 5
-   personal: 30
-   sillas: 15
-   pantallas_llama_turnos= 4
-   ventanillas= 12
+  "atributos"
+  cajeros: 5
+  personal: 30
+  sillas: 15
+  pantallas_llama_turnos= 4
+  ventanillas= 12
    
   def __init__(self):
      print("Constructor de bancos")
