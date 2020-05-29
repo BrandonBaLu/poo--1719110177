815c61231c032658ad471550cb380ef5409e42a6
Brandon Balderas Lucas <1719110177@utectulancingo.edu.mx>
2020-05-29 21:06:59 +0000

    1

--- semana-3/cajero.py
+++ semana-3/cajero.py
@@ -1,5 +1,4 @@
-class calculadora:
-
+class banco:
    
   "atríbutos"
   cajeros= 5
@@ -55,47 +54,3 @@ elektra.retirar()
 elektra.depositar()
 elektra.prestamos()
 elektra.abonos()
-
-  
-  
-  
-  
-  funciones= 500
-  multiplicacion="multiplicacion"
-  teclas= 50
-  tipo= "cientifica"
-  modelo= "126Clc"
-  marca= "CASIO"
-  tama= "grande"
-  forma="rectangular"
-  color="roja"
-  '''metodos'''
-  def prender(self):
-    print("Encender")
-  def multiplica(self):
-    print("multiplica") 
-  def sumar(self):
-    print("sumar")
-  def restar(self):
-    print("restar")
-  def apagar(self):
-    print("Apagado automatico")
-  
-  def _init_(self):
-    print("Calculadora Cientifica")
-    print("funciones: "+ str(self.funciones))
-    print("Realiza: "+str(self.multiplicacion))
-    print(self.teclas)
-    print(self.tipo)
-    print(self.modelo)
-    print(self.marca)
-    print(self.tama)
-    print(self.forma)
-    print(self.color)
-    
-objeto=calculadora()
-objeto.prender
-objeto.multiplica
-objeto.sumar
-objeto.restar
-objeto._init_()
