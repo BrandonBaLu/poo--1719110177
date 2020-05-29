c889c4bbb6b9824c5b4aa00124c6bf32c79af321
Brandon Balderas Lucas <1719110177@utectulancingo.edu.mx>
2020-05-29 22:21:56 +0000

    2

--- semana-3/cajero.py
+++ semana-3/cajero.py
@@ -1,21 +1,22 @@
-class calculadora:
+class cajero:
 
    
-  "atríbutos"
-  funciones= "funciones"
-  multiplicacion="multiplicacion"
-  teclas= 50
-  tipo= "cientifica"
-  forma="rectangular"
+  #atríbutos"
+
+  funciones= 22
+  gabeta= 1
+  gabeta_billetes= 1
+  impresora= 1
+  teclas= 20
   
   #métodos
 
-  def prender(self):
-    print("Encender")
-
-  def apagar(self):
-    print("apagar")
-
+  def imprimir(self):
+    print("imprime")
+  
+  def pagar(self):
+    print("cierra gabeta")
+  
   
   def __init__(self):
     pass
@@ -26,71 +27,41 @@ class casio(calculadora):
   
   #atríbutos
   
-  funciones_c= 500
-  color="roja"
+  pantalla= "pantalla touch"
+  color="gris"
 
   #métodos
   
-  def  graficar(self):
-    print("graficar")
-    
-  def dividir(self):
-    print("dividir")
+  def cambio(self):
+    print("cambiar dinero")
+  
+  def servicios(self):
+    print("abre gabeta")
 
   def __init__(self):
     print("constructor de calculadora casio_126Clc")
     pass
 
-casio_126Clc = casio()
+cajero_BBVA = casio()
 
-print(casio_126Clc.funciones)
-print(casio_126Clc.multiplicacion)
-print("teclas= "+str(casio_126Clc.teclas))
-print("tipo= "+str(casio_126Clc.tipo))
-print("forma "+str(casio_126Clc.forma))
-print("funciones casio 126Clc= "+str(casio_126Clc.funciones_c))
-print("color= "+str(casio_126Clc.color))
-
-
-
-casio_126Clc=casio()
-casio_126Clc.prender()
-casio_126Clc.apagar()
-casio_126Clc.graficar()
-casio_126Clc.dividir()
+print(cajero_BBVA .funciones)
+print("gabeta "+str(cajero_BBVA .gabeta))
+print("gabeta para billetes= "+str(cajero_BBVA .gabeta_billetes))
+print("impresora "+str(cajero_BBVA .impresora))
+print("teclas "+str(cajero_BBVA .teclas))
+print(cajero_BBVA.pantalla)
+print("color= "+str(cajero_BBVA .color))
 
 
-  class cajero:
-  funciones= 22
-  gabeta= "si"
-  gabeta_billetes= 1
-  retirar= "cantidad a retirar"
-  depositar="juvenil"
-  nombre= "Brandon BaLu"
+cajero_BBVA .imprimir()
+cajero_BBVA .cambio()
+cajero_BBVA .servicios()
+cajero_BBVA .pagar()
+
+
   
-  def imprimir(self):
-    print("imprime")
-  def cambio(self):
-    print("cambiar dinero")
-  def servicios(self):
-    print("abre gabeta")
-  def pagar(self):
-    print("cierra gabeta")
-  def resivir(self):
-    print("contar dinero")
- 
-  def _init_(self):
-    print("Futbolista")
-    print(self.funciones)
-    print(self.gabeta)
-    print(self.gabeta_billetes)
-    print(self.retirar)
-    print(self.depositar)
-    print(self.nombre)
 
-objeto = cajero()
-objeto.imprimir()
-objeto.cambio()
-objeto.servicios()
-objeto.pagar()
-objeto.resivir()
+
+
+
+
