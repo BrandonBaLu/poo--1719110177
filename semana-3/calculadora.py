4f27bb076d5e8474dcfa1b090608fb6fcaad62bc
Brandon Balderas Lucas <1719110177@utectulancingo.edu.mx>
2020-05-29 21:51:25 +0000

    4

--- semana-3/calculadora.py
+++ semana-3/calculadora.py
@@ -30,57 +30,30 @@ class casio(calculadora):
   color="roja"
 
   #métodos
-
-  def sumar(self):
-    print("sumar")
   
+  def  sumar(self):
+    print("sumar")
+
   def restar(self):
     print("restar")
 
   def __init__(self):
-    print("constructor calculadora casio 126Clc")
+    print("constructor de un avíon Boeing 747")
     pass
 
 casio_126Clc = casio()
 
 print(casio_126Clc.funciones)
 print(casio_126Clc.multiplicacion)
-print("teclas= "+str(casio_126Clc.teclas)
+print("teclas= "+str(casio_126Clc.teclas))
 print("tipo= "+str(casio_126Clc.tipo))
 print("forma "+str(casio_126Clc.forma))
 print("funciones casio 126Clc= "+str(casio_126Clc.funciones_c))
 print("color= "+str(casio_126Clc.color))
 
 casio_126Clc=calculadora()
-casio_126Clc.prender
-casio_126Clc.multiplica
-casio_126Clc.sumar
-casio_126Clc.restar
+casio_126Clc.prender()
+casio_126Clc.apagar()
+casio_126Clc.restar()
+casio_126lc.sumar()
 
-  
-  
-  
-  
-  
-  modelo= "126Clc"
-  marca= "CASIO"
-  
-  '''metodos'''
-  
-  def multiplica(self):
-    print("multiplica") 
-  
-  
-  
-  def _init_(self):
-    print("Calculadora Cientifica")
-    print("funciones: "+ str(self.funciones))
-    print("Realiza: "+str(self.multiplicacion))
-    print(self.teclas)
-    print(self.tipo)
-    print(self.modelo)
-    print(self.marca)
-    print(self.tama)
-    print(self.forma)
-    print(self.color)
-    
