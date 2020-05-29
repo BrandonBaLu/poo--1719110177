c02554b223fd73de56e87af6ff6545dd0979c17b
Brandon Balderas Lucas <1719110177@utectulancingo.edu.mx>
2020-05-29 22:40:58 +0000

    1.1

--- semana-3/classroom.py
+++ semana-3/classroom.py
@@ -1,12 +1,64 @@
 class classroom:
- Color="anaranjado"
- Correo="1719110177@utectulancingo.edu.mx"
- nombre_del_alumno="Brandon Balderas Lucas"
- Institucion="UTEC"
- Numero_tareas= "10"
- Publicacion_tarea= "Lunes"
- Fecha_entrega=" Domingo"
+   
+  "atríbutos"
+  Color="anaranjado"
+  Correo="1719110177@utectulancingo.edu.mx"
+  nombre_del_alumno="Brandon Balderas Lucas"
+  Institucion="UTEC"
+  Numero_tareas= "10"
+  
+  #métodos
 
+  def prender(self):
+    print("Encender")
+
+  def apagar(self):
+    print("apagar")
+
+  
+  def __init__(self):
+    pass
+
+        
+
+class classrom_salvador(classroom):
+  
+  #atríbutos
+  
+  Publicacion_tarea= "Lunes"
+  Fecha_entrega=" Domingo"
+
+  #métodos
+  
+  def  graficar(self):
+    print("graficar")
+    
+  def dividir(self):
+    print("dividir")
+
+  def __init__(self):
+    print("constructor de calculadora casio_126Clc")
+    pass
+
+casio_126Clc = casio()
+
+print(casio_126Clc.funciones)
+print(casio_126Clc.multiplicacion)
+print("teclas= "+str(casio_126Clc.teclas))
+print("tipo= "+str(casio_126Clc.tipo))
+print("forma "+str(casio_126Clc.forma))
+print("funciones casio 126Clc= "+str(casio_126Clc.funciones_c))
+print("color= "+str(casio_126Clc.color))
+
+
+
+casio_126Clc=casio()
+casio_126Clc.prender()
+casio_126Clc.apagar()
+casio_126Clc.graficar()
+casio_126Clc.dividir()
+ 
+ 
 
  '''Metodos'''
  def Entregar(self): 
