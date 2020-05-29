bccd601396c1624f46daa86b61978565b6b4dbcd
Brandon Balderas Lucas <1719110177@utectulancingo.edu.mx>
2020-05-29 21:33:44 +0000

    2

--- semana-3/calculadora.py
+++ semana-3/calculadora.py
@@ -13,8 +13,8 @@ class calculadora:
   def prender(self):
     print("Encender")
 
-  def depositar(self):
-    print("depositar")
+  def apagar(self):
+    print("apagar")
 
   
   def __init__(self):
@@ -26,7 +26,7 @@ class casio(calculadora):
   
   #atríbutos
   
-  tamaño= "grande"
+  funciones_c= 500
   color="roja"
 
   #métodos
@@ -43,13 +43,13 @@ class casio(calculadora):
 
 casio_126Clc = casio()
 
-print("abierto hasta las= "+str(casio_126Clc.abierto_tarde))
-print(casio_126Clc.entrega)
-print("cajeros= "+str(casio_126Clc.cajeros))
-print("personal= "+str(casio_126Clc.personal))
-print("sillas= "+str(casio_126Clc.sillas))
-print("pantallas llama turnos= "+str(casio_126Clc.pantallas_llama_turnos))
-print("ventanillas= "+str(casio_126Clc.ventanillas))
+print(casio_126Clc.funciones)
+print(casio_126Clc.multiplicacion)
+print("teclas= "+str(casio_126Clc.teclas)
+print("tipo= "+str(casio_126Clc.tipo))
+print("forma "+str(casio_126Clc.forma))
+print("funciones casio 126Clc= "+str(casio_126Clc.funciones_c))
+print("color= "+str(casio_126Clc.color))
 
 casio_126Clc=calculadora()
 casio_126Clc.prender
