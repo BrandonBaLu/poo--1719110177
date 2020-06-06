5c28a1fe9a54ccfd48bb038f43558f1f1421c26b
Brandon Balderas Lucas <1719110177@utectulancingo.edu.mx>
2020-06-06 19:33:01 +0000

    1.2

--- semana-4/strings.py
+++ semana-4/strings.py
@@ -1,9 +1,45 @@
 class cadenas:
+	cadena = ""
 
-  cadena_1=input("ingresa la cadena a leer\n")
+	def __init__(self, cadena):
+		self.cadena = cadena
 
-  def __init__(self):
-    pass
+	def imprimirPorSeparado(self):
 
-objeto=cadenas()
-print(objeto.cadena_1)
+		for caracter in self.cadena:
+			print(caracter)
+
+	def imprimirTipoDato(self):
+		dato = ""
+		print("La cadena por separado")
+		for caracter in self.cadena:
+			if caracter.isdecimal() == True:
+				dato = "Numero"
+			elif caracter.isalpha() == True:
+				dato = "Letra"
+			elif caracter.isdecimal() == False and caracter.isdigit() == False:
+				dato = "Simbolo"
+
+			print("El siguiente : " + caracter + " es un: " + dato)
+
+	def imprimirLongiud(self):
+		print("La longitud de la cadena es de :" + str(len(self.cadena)) +
+		      " caracteres")
+
+	def imprimirLonsinSpacio(self):
+		quitarEspacio = self.cadena.replace(" ", "")
+		print("La longitud de la cadena sin espacios es de :" +
+		      str(len(quitarEspacio)) + " caracteres")
+		print("La cadena sin espacios: " + quitarEspacio)
+
+
+eleccion = "S"
+while eleccion == "S" or eleccion == "s":
+	cadena = input("Ingresa el texto:\n")
+	objetocadenas = cadenas(cadena)
+	print(objetocadenas.cadena)
+	objetocadenas.imprimirLongiud()
+	objetocadenas.imprimirPorSeparado()
+	objetocadenas.imprimirTipoDato()
+	objetocadenas.imprimirLonsinSpacio()
+	eleccion = input("¿Desea analizar otra cadena s/n\n")
