78123fbd1076e8b8435564786712c2c3d34e3f1d
Brandon Balderas Lucas <1719110177@utectulancingo.edu.mx>
2020-06-14 22:01:47 +0000

    3

--- semana-5/Pal.py
+++ semana-5/Pal.py
@@ -1,33 +1,33 @@
-import unidecode  
-class palindromo: 
-  texto =""
+import unidecode  #importe la libreria que necesitaba para que lea los caracteres  
+class palindromo: #inserte la clase llamada palindromo
+  texto =""#declare mis variables donde se guardara el texto que ingrese el usuario, declare las vocales e iguale otra variable de texto para continuar programando
   vocales="aeiou" 
   txt=texto 
-  espacios=" "
+  espacios=" "#declare la variable espacios que ocuparia en mi ciclo que contara el numero de espacios que hay
 
-  def __init__(self,texto): 
-    self.texto=texto1    
-    self.texto=texto1.replace(" ", "").lower().replace(".","")
+  def __init__(self,texto): #definí mi constructor  texto
+    self.texto=texto1  #igaule mi variable texto a otra variable que mas abajo ocupe para otras funciones 
+    self.texto=texto1.replace(" ", "").lower().replace(".","") #remplace los espacios, puntos y mayusculas a minusculas
 
    
   
-  def palindromo(self):
+  def palindromo(self): #definí mi funcion palindromo 
     
-    lista=[]
-    listaaux=[]
+    lista1=[] #declare dos listas que guardara los caracteres 
+    listaux=[]
    
-    cont=0
+    cont=0 #definí mis contadores en forma global
     contador1=0
-    aux=0
-    resultado=""
+    aux=0 
+    resultado="" #variables globales
     
-    for indice in range(len(self.texto)):
-      caracter = self.texto[indice]
-      lista.append(caracter)
-      listaaux.append(caracter)
-      cont=cont+1 
+    for indice in range(len(self.texto)): #inicie un blucle en un rango del texto que el usuario ingrese 
+      caracter = self.texto[indice] #dentro de mi variable indice iguale mis caracteres con texto
+      lista1.append(caracter)  #listas globales 
+      listaux.append(caracter)
+      cont=cont+1  #aumente mi contador 
     for i in range(cont-1,-1,-1): 
-      if lista[aux]==listaaux[i]:
+      if lista1[aux]==listaux[i]:
         resultado="Es palindromo"
       else:
         resultado="No palindromo"
@@ -45,7 +45,7 @@ class palindromo:
 
 
   def vocales(self):
-    vocal=["a","e","i","o","u","á","é","í","ó","ú"]
+    vocal=["a","e","i","o","u","á","é","í","ó","ú"] 
     cont=0
     for i in vocal:
       for j in txt:
@@ -67,7 +67,7 @@ while  repetir=="S" or repetir=="s":
       contador1+=1
   print("El total de espacios dentro del texto es:",contador1)   
   
-  if texto.isalpha() == True:
+  if texto.isalpha() == True: #evalúe si es verdad que iterara los 
     texto=unidecode.unidecode(texto1)
   objetopalindromo= palindromo(texto1)
   objetopalindromo.palindromo()
