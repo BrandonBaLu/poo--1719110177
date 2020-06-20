59399593726829f075eed13308a7b9ac813c2c5f
Brandon Balderas Lucas <1719110177@utectulancingo.edu.mx>
2020-06-20 21:36:29 +0000

    1.1

--- semana-5/cesar.py
+++ semana-5/cesar.py
@@ -1,15 +1,33 @@
 class Ascii():
+  texto=""
+  if texto==texto.upper () :
+    abc="ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+  
+  else:
+    abc="abcdefghijklmnñopqrstuvwxyz"
+  
+  tipo=int(input("Ingrese el tipo de trabajo a realizar |\n1:Encriptar 2:Desencriptar|\ningrese numero\n"))
+  texto =""
 
   def __init__(self):
     pass
-  
-  def ascii(self,caracter):
-    return ord(caracter)
+  if tipo==1:
+    def ascii(self,texto):
+      cifrado=0
+      texto=input("ingrese su cadena de texto\n")
+      for c in texto:
+        if c in abc:
+          cifrad += abc(abc.index(c)+k%(len(abc)))
+        else:
+          cifrad+=c
+          return ord(texto)
 
   def caracter(self, ascii):
     return chr(ascii)
 
 objeto=Ascii()
 
-print(objeto.ascii('@'))
+print(objeto.ascii(caracter))
 print(objeto.caracter(75))
+
+
