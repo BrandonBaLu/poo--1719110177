2a166c86f69d0988896ccc8a59923e1dd75b4f1d
Brandon Balderas Lucas <1719110177@utectulancingo.edu.mx>
2020-06-20 19:51:26 +0000

    1

--- semana-5/cesar.py
+++ semana-5/cesar.py
@@ -1,4 +1,15 @@
-class cesar:
-  texto=""
-  
+class Ascii():
 
+  def __init__(self):
+    pass
+  
+  def ascii(self,caracter):
+    return ord(caracter)
+
+  def caracter(self, ascii):
+    return chr(ascii)
+
+objeto=Ascii()
+
+print(objeto.ascii('@'))
+print(objeto.caracter(75))
