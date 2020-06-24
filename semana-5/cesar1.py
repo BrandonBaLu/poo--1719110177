7df082072dc22e6b2cc7bba2d942153ac17aa68f
Brandon Balderas Lucas <1719110177@utectulancingo.edu.mx>
2020-06-24 20:37:37 +0000

    2.1

--- semana-5/cesar1.py
+++ semana-5/cesar1.py
@@ -1,22 +1,25 @@
 class cesar:
-   
-  
-  def __init__(texto):
+
+  def __init__(self):
     pass
 
   def ascii(self):
     texto =input("ingresa tu texto\n")
     for texto in texto:
-        print("El valor de {} es {}".format(texto, ord(texto)))
+      print("El valor de {} es {}".format(texto, ord(texto)))
   
   def descodificar (self):
     numeros=int(input("ingresa tus valores en numeros enteros\n"))
     for numero in numeros:
-      print("El carácter que representa a {} es {}".format(numero, chr(numero))
-      
+      print("El carácter que representa a {} es {}".format(numero, chr(numero)))
+
+
 repetir="s"
 while repetir=="s" or repetir=="S":
   objetoCesar = cesar()
-  objetoCesar.ascii()
-  objetoCesar.descodificar
+  tipo=int(input("que deseas hacer\n|1:cifrar\n2:descifrar\n"))
+  if tipo==1:
+    objetoCesar.ascii()
+  if tipo==2:
+    objetoCesar.descodificar()
   repetir=input ("¿Desea repetir? S/N\n")
