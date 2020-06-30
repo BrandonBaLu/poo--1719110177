7eca6fd94d7fd37735a0faa54473e1dff166ba13
Brandon Balderas Lucas <1719110177@utectulancingo.edu.mx>
2020-06-30 16:12:56 +0000

    1.1

--- semana-5/open.py
+++ semana-5/open.py
@@ -1,37 +1,39 @@
-class archivo_vscesar: #clase principal#
-  def _init_(self): #metodo constructor#
+class cesar:
+  def _init_(self): 
     pass
-  def cifardesif(self):
-    devolver="s" #Defina variable devolver#
-    while devolver== "S" or devolver=="s": #si le elige s o S se vuelve a correr el programa#
-      cif= "" #se alamacena el texto cifrar#
-      desif= "" #se alamcena e texto a desifrar #
-      textoarch= input("Nombre del archivo: ") #se pide de archivo#
-      abrir= open(textoarch, "r") #abrimos el archivo y se usa la letra r porque es la inical de la palabra read y da alucion a leer#
-      leer= abrir.read() #se lee el archivo#
-      print(leer) #se imprieme lo que contiene l avariable leer#
- 
-      preguntar= input("¿Cifrar o desifrar?" ) #Se pregunta si desifra o cifra#
-      if preguntar== "Cifrar" or preguntar== "cifrar": #cualquiera que se la obcion se ejecuta lo siguiente #
- 
-        abrir= open(textoarch, "a") #se abre el arcivo y se adjunta por eso la letra a#
- 
+  def tipo(self):
+    repetir ="s" 
+    while repetir == "S" or repetir =="s": #si le elige s o S se vuelve a correr el programa#
+      codificar= "" 
+      descifrar = "" 
+      print ("=======++++++++++++++++=======") 
+      archivo= input("Escribe el nombre del archivo:\n=======++++++++++++++++=======\n") #se pide de archivo#
+      abrir= open(archivo, "r") 
+      leer= abrir.read() 
+      print(leer) 
+      print("********")
+      respuesta= int(input("====¦1:Codificar¦=======\n====¦2:descodificar¦====\n"+"********\n"))
+      if respuesta==1: #cualquiera que se la obcion se ejecuta lo siguiente #
+        abrir= open(archivo, "a") #se abre el arcivo y se adjunta por eso la letra a#
         for cambiar in leer: #se define el rango#
-          abesedario= ord(cambiar) #se hace un cambio de caracteres#
-          cif+= chr(abesedario-1)#se resta 1 justo como funcina el codigo ASCII#
-        abrir.write(cif) #Se lee la variable a codificar#
-        print(cif) #imprime el texto guardo pero ya cifrado#
-        abrir.close() #cierra el archivo#
-      if preguntar== "desifrar" or preguntar== "Desifrar": #cualquiera que se opcion corre las siguientes lineas#
-        abrir.open(textoarch, "a") #a de adjuntar asi que adjunta el archivo#
-        for cambiar in leer: #los cambia al leguaje normal#
-          abesedario= ord(cambiar) #se cmabia allenguaje matural#
-          desif += chr(abesedario+1) #le suma uno para desifrarlo#
-        abrir.write(desif) #lo escibre en el archivo de texto#
-        abrir.close() #cierra el archivo#
-        print(desif) #imprime el desifrado#
-      devolver=input("¿Quiere repetir?" ) #pregunta si quieres repetir la operacion#
-      if devolver== "N" or devolver== "n": #cualquiera que sea la opcion termina el proceso#
-        break
-objeto=archivo_vscesar() #se menciona la clase principal#
-objeto.cifardesif() #se llama al objeto#
+          letras= ord(cambiar) #se hace un cambio de caracteres#
+          codificar+= chr(letras-2)#se resta 1 justo como funcina el codigo ASCII#
+        abrir.write(codificar) #Se lee la variable a codificar#
+        print("\n",codificar ) 
+        abrir.close() 
+      if respuesta==2:
+        abrir= open(archivo, "a") #se abre el arcivo y se adjunta por eso la letra a#
+        for cambiar in leer: #se define el rango#
+          letras= ord(cambiar)  
+          descifrar+= chr(letras+2)
+        abrir.write(descifrar ) 
+        print("\n",descifrar ) 
+        abrir.close() 
+      repetir =input("¿desea repetir?\nS/N\n" ) #pregunta si quieres repetir la operacion#
+      if repetir == "N" or repetir == "n":
+          print ("==============================") 
+          print("*Gracias por usar codigo cesar*")
+          print ("==============================") 
+          break
+objeto=cesar() 
+objeto.tipo()
